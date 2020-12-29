b9d4bd51f15c10e631d97dc414e8d9fadfa5343a
OttoAndrey <30920379+OttoAndrey@users.noreply.github.com>
2020-12-29 23:27:56 +0700

    Update settings.py

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'deal.apps.DealConfig',
 ]
 
 MIDDLEWARE = [
@@ -78,13 +79,6 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {'default': dj_database_url.config()}
 
