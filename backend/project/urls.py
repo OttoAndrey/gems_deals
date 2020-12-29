e8ec268a5d26310097d683163721530bf2b6fc59
OttoAndrey <30920379+OttoAndrey@users.noreply.github.com>
2020-12-29 23:28:16 +0700

    Add DealView to urls

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from deal.views import DealView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('deal/', DealView.as_view(), name='deal'),
 ]
