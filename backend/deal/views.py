62154efbd8db1a1d76c1f0f2fdd9f0598ac89f9e
ray <ottotruepro@gmail.com>
2021-01-03 20:26:17 +0700

    Add comments to views

--- backend/deal/views.py
+++ backend/deal/views.py
@@ -12,24 +12,36 @@ class DealView(APIView):
     def get(self, request):
         most_spends_users = Users.objects.most_spends()[:5].prefetch_related('deals')
 
+        # Формируем словарь,
+        # где ключ-покупатель, значение-названия камней,
+        # которые он покупал (без повторений).
         users_gems = {}
         for user in most_spends_users:
-            user_gems = set(deal.item.title for deal in user.deals.select_related('item'))
+            user_gems = set(deal.item for deal in user.deals.select_related('item'))
             users_gems[user] = user_gems
 
+        # Начинаем формировать список пользователей,
+        # который вернем в ответ на запрос.
         users = []
         for user in most_spends_users:
+            # Перебираем самых затратных покупателей еще раз
+            # и убираем их словаря с камнями текущего покупателя.
             user_gems = users_gems.pop(user)
 
+            # Создаём список со всеми камнями других пользователей.
             other_user_gems = []
             for gems in users_gems.values():
                 other_user_gems.extend(gems)
 
+            # Если камень, который покупал текущий пользователь,
+            # будет среди камней других пользователей,
+            # тогда добавляем его в ответ на запрос.
             cross_gems = []
             for user_gem in user_gems:
                 if user_gem in other_user_gems:
-                    cross_gems.append(user_gem)
+                    cross_gems.append(user_gem.title)
 
+            # Возвращаем пользователя обратно в словарь.
             users_gems[user] = user_gems
             users.append({'username': user.username,
                           'spent_money': user.total_sum,
